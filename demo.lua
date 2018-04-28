6bd11f94c63d5dc73041f161fd913f8db60dc0b2
Erfan Noury <erfan.noury@live.com>
2018-04-28 19:18:21 -0400

    Fix formatting and cleanup files

--- demo.lua
+++ demo.lua
@@ -1,12 +1,12 @@
 -- This is a demo file for performing detection given some proposals
 -- Require the detection package
-require 'detection'
+require "detection"
 -- Require the utils class
 local utils = detection.GeneralUtils()
 
 -- Paths
-local image_path = './data/demo/test.jpg'
-local selective_search_path = './data/demo/proposals.mat'
+local image_path = "./data/demo/test.jpg"
+local selective_search_path = "./data/demo/proposals.mat"
 local param_path = config.model_weights
 local model_path = config.model_def
 
@@ -14,25 +14,24 @@ local model_path = config.model_def
 local model_opt = {}
 model_opt.fine_tuning = false
 model_opt.test = true
-if config.dataset== 'MSCOCO' then
-   model_opt.nclass = 80
+if config.dataset == "MSCOCO" then
+    model_opt.nclass = 80
 else
-   model_opt.nclass = 20
+    model_opt.nclass = 20
 end
 
-network = detection.Net(model_path,param_path,model_opt)
+network = detection.Net(model_path, param_path, model_opt)
 
 -- Creating the network wrapper
 local network_wrapper = detection.NetworkWrapper(network)
 network_wrapper:evaluate()
 
 -- Loading proposals from file
-local proposals = matio.load(selective_search_path)['boxes']
+local proposals = matio.load(selective_search_path)["boxes"]
 proposals = proposals:add(1)
 
 -- Loading the image
 local im = image.load(image_path)
-
 
 -- detect !
 local scores, bboxes = network_wrapper:detect(im, proposals)
@@ -40,7 +39,26 @@ local scores, bboxes = network_wrapper:detect(im, proposals)
 -- visualization
 local threshold = 0.5
 -- classes from Pascal used for training the model
-local cls = {'aeroplane','bicycle','bird','boat','bottle','bus','car',
-  'cat','chair','cow','diningtable','dog','horse','motorbike',
-  'person','pottedplant','sheep','sofa','train','tvmonitor'}
-utils:visualize_detections(im,bboxes,scores,threshold,cls)
+local cls = {
+    "aeroplane",
+    "bicycle",
+    "bird",
+    "boat",
+    "bottle",
+    "bus",
+    "car",
+    "cat",
+    "chair",
+    "cow",
+    "diningtable",
+    "dog",
+    "horse",
+    "motorbike",
+    "person",
+    "pottedplant",
+    "sheep",
+    "sofa",
+    "train",
+    "tvmonitor"
+}
+utils:visualize_detections(im, bboxes, scores, threshold, cls)
--- detection.lua
+++ detection.lua
@@ -1,12 +1,12 @@
-require 'image'
-require 'cudnn'
-require 'inn'
-require 'nn'
-require 'torch'
-require 'xlua'
-matio = require 'matio'
-tds = require 'tds'
-config = dofile 'config.lua'
+require "image"
+require "cudnn"
+require "inn"
+require "nn"
+require "torch"
+require "xlua"
+matio = require "matio"
+tds = require "tds"
+config = dofile "config.lua"
 config = config.parse(arg)
 cutorch.setDevice(config.GPU_ID)
 
@@ -16,28 +16,28 @@ torch.manualSeed(3)
 detection = {}
 
 -- General Utilities
-torch.include('detection','utils/GeneralUtils.lua')
+torch.include("detection", "utils/GeneralUtils.lua")
 
 -- Detection roidb modules
-torch.include('detection','ROI/ROI.lua')
-torch.include('detection','ROI/ROIPooling.lua')
+torch.include("detection", "ROI/ROI.lua")
+torch.include("detection", "ROI/ROIPooling.lua")
 -- Detection utilities
-torch.include('detection','utils/InputMaker.lua')
-torch.include('detection','utils/CaffeLoader.lua')
-torch.include('detection','utils/CaffeModelConverter.lua')
-torch.include('detection','utils/heap/heap.lua')
-
+torch.include("detection", "utils/InputMaker.lua")
+torch.include("detection", "utils/MaryamNetInputMaker.lua")
+torch.include("detection", "utils/CaffeLoader.lua")
+torch.include("detection", "utils/CaffeModelConverter.lua")
+torch.include("detection", "utils/heap/heap.lua")
 
 -- -- Detection datasets
-torch.include('detection','datasets/DataSetDetection.lua')
-torch.include('detection','datasets/DataSetPascal.lua')
-torch.include('detection','datasets/DataSetCoco.lua')
+torch.include("detection", "datasets/DataSetDetection.lua")
+torch.include("detection", "datasets/DataSetPascal.lua")
+torch.include("detection", "datasets/DataSetCoco.lua")
 
--- Detection Network training and Testing 
+-- Detection Network training and Testing
 -- torch.include('detection','DB_tester.lua')
-torch.include('detection','network/NetworkWrapper.lua')
-torch.include('detection','network/Net.lua')
-torch.include('detection','train/Batcher.lua')
-torch.include('detection','train/ParallelTrainer.lua')
-torch.include('detection','train/SequentialTrainer.lua')
-torch.include('detection','train/WeightedSmoothL1Criterion.lua')
+torch.include("detection", "network/NetworkWrapper.lua")
+torch.include("detection", "network/Net.lua")
+torch.include("detection", "train/Batcher.lua")
+torch.include("detection", "train/ParallelTrainer.lua")
+torch.include("detection", "train/SequentialTrainer.lua")
+torch.include("detection", "train/WeightedSmoothL1Criterion.lua")
